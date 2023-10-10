15e4fd0a3b569f3169ce7b540862e274dcd8f412
FullStack <mazaromanana@gmail.com>
2023-10-10 12:26:44 +0200

    10102023

--- proyectoJavaScript/js/02-tiposDeDatos.js
+++ proyectoJavaScript/js/02-tiposDeDatos.js
@@ -34,3 +34,64 @@ let ejString1 ="STRING 1",
 console.log("Metodo length", ejString1.length);
 console.log("Metodo indexOf", ejString2.indexOf("2")); 
 console.log("Metodo includes", ejString3.includes("3"));
+
+
+//NUMBER
+let numero1= 100;
+let numero2=200.25;
+let numero3="200";
+let numero4=2/3;
+
+//OPERACIONES 
+
+console.log("Typeof numero1 100", typeof numero1);
+console.log("Typeof numero2 200.25", typeof numero2);
+console.log("Typeof numero3 200", typeof numero3);
+console.log("Typeof numero4 2/3", typeof numero4);
+
+
+console.log("SUMA", numero1+numero2);
+console.log("RESTA", numero1-numero2);
+console.log("MULTIPLICACION", numero1*numero2);
+console.log("DIVISION", numero1/numero2);
+console.log("PORCENTAJE", numero1%numero2);
+
+// METODOS PARA NUMBER
+
+//EJERCICIO NUMERO ALEATORIO ENTRE 0 Y 30
+
+let resultado= Math.round(Math.random()*30);
+console.log("Resultado numero aletario:", resultado);
+
+// EJERCICIO : CREAR UNA VARIABLE QUE SE LLAME DIAMETRO Y OTRA QUE SE LLAME AREA IMPLEMENTAR LA OPERACION MATEMATICA QUE ME CALCULA EL AREA EN FUNCION DEL DIAMETRO YMOSTRARLO POR LA PANTALLA 
+
+let diametro=5;
+let area=Math.PI*Math.pow(diametro/2,2);
+
+console.log("Resultado",area);
+
+// ORDEN MATEMTICO
+
+let resultadoOrden = 20 + 30 * 2;
+console.log("MOSTRAR EL ORDEN DE LAS OPERACIONES", resultadoOrden);
+
+let resultadoOrden2 =(20 + 30)*2;
+console.log("MOSTRAR EL ORDE LAS OPERACIONES 2", resultadoOrden2);
+
+
+//EJERCICIO CALCULAR EL DESCUENTO Y EL TOTAL A PAGAR 
+
+let descuento=40,
+    prenda1=120,
+    prenda2=85,
+    prenda3=24,
+    total= 120+85+24;
+    aPagar= descuento*total/100;
+    ahorro= total-aPagar;
+
+    console.log("TOTAL:",total);
+    console.log("A PAGAR", aPagar);
+    console.log("AHORRO", ahorro);
+    
+
+    
