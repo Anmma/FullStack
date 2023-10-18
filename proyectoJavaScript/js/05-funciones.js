2d6c3e300091588629a91c9b910c95553b654f71
FullStack <mazaromanana@gmail.com>
2023-10-18 14:31:12 +0200

    18102023

--- proyectoJavaScript/js/05-funciones.js
+++ proyectoJavaScript/js/05-funciones.js
@@ -65,7 +65,10 @@ tweet("Texto de prueba para contar los caracteres");
 
 // CREAR UNA FUNCIÓN QUE DADO UN TEXTO Y UNA PALABRA, ME DIGA SI
 // LA PALABRA ESTA CONTENIDA EN EL TEXTO
-
+function tweet(textoTweet){
+    console.log("La longitud del tweet es: ", textoTweet.length);
+}
+ 
 function busqueda(texto, palabra){
     console.log("Buscamos si palabra esta incluida en el texto, lo esta? ", texto.includes(palabra));
 }
@@ -93,77 +96,228 @@ restar2(5);
 restar2()
 
 
-// EJERCICIO
- 
-// CREAR UNA FUNCIÓN QUE SOLUCIONE LAS ECUACIONES DE SEGUNDO GRADO
-// X=(-B +-SQRT(B*B -4*A*C))/2*A
- 
-// A, B, C
-// ecuacionSegundo
-
-
-function ecuacionSegundo(A = 0, B = 0, C = 0) {
-    console.log("Resultado operacion segundo Grado +:", (-B + Math.sqrt(B * B - 4 * A * C)) / (2 * A));
-    console.log("Resultado operacion segundo Grado -:", (-B - Math.sqrt(B * B - 4 * A * C)) / (2 * A));
-};
-
-ecuacionSegundo(2, 10, 1);
-ecuacionSegundo(2, 10);
-
-
 // FUNCIONES QUE RETORNAN VALORES
 
-function multiplicar(n1, n2) {
-    return n1 * n2; // ME RETORNA UN VALOR
+
+function multiplicar (n7, n8){
+    return n7 * n8;
 }
 
-let valorRetornado = multiplicar(2, 5);
-console.log("EL VALOR RETORNADO ES:", valorRetornado);
-
+let resultadoMultiplicar = multiplicar (2, 5); // resultdoMultiplicar es una variable creada que almaceranara mi funcion
+console.log("EL VALOR RETORNADO ES ", resultadoMultiplicar);
 
 //MAS COMPLEJA
 
-let total = 0;
 
-function agregarCarrito(precio) {
-    return total += precio;
-}
+// let totalCarrito  = 0;
+// function agregarCarrito (precio){
+//     return total += precio;
+// }
 
-total = agregarCarrito(200);
-total = agregarCarrito(500);
-total = agregarCarrito(600);
+// agregarCarrito (20);
+// agregarCarrito (50);
+// agregarCarrito (60);
 
-console.log("EL TOTAL A PAGAR ES DE:", total);
+// console.log("EL TOTAL A PAGAR ES DE: ", total);
+
+
 
 // EJERCICIO
 // REALIZAR UNA FUNCIÓN QUE CALCULES EL IVA QUE SE INDIQUE DE LOS
 // PRODUCTOS ANTERIORES UNO A UNO Y LOS SUME
 
 
+// let impuesto=0;
+// let IVA=21;
 
-let impuesto=0;
-let IVA=21;
+// function calcularImpuesto(precio=0){
+//     return impuesto+=(precio*IVA/100)
+// }
 
-function calcularImpuesto(precio=0){
-    return impuesto+=(precio*IVA/100)
+// impuesto = calcularImpuesto(200);
+// impuesto = calcularImpuesto(500);
+// impuesto = calcularImpuesto(600);
+
+// console.log("EL TOTAL DE IMPUESTOS A PAGAR ES DE:",impuesto)
+
+
+
+// let totalConImpuestos = 0;
+
+// function sumarImpuestos(precio, impuesto) {
+//     return totalConImpuestos += precio * (1+impuesto / 100)
+// }
+
+// sumarImpuestos(200, 10);
+// sumarImpuestos(300, 21);
+// sumarImpuestos(500, 5);
+
+// console.log("EL TOTAL CON IMPUESTOS ES DE:", totalConImpuestos);
+
+
+
+
+// EJERCICIO CALCULAR EL DESCUENTO QUE SE INDIQUE Y LOS SUME
+
+
+let descuento = 0.2;
+let totalAhorro = 0;
+
+function calcularDescuento(z){
+        totalAhorro += z * descuento;
 }
 
-impuesto = calcularImpuesto(200);
-impuesto = calcularImpuesto(500);
-impuesto = calcularImpuesto(600);
-
-console.log("EL TOTAL DE IMPUESTOS A PAGAR ES DE:",impuesto)
-
+calcularDescuento  (100) ;
+calcularDescuento  (150) ;
+calcularDescuento  (170) ;
+console.log("EL TOTAL DE MI AHORRO SERA DE: ", totalAhorro);
 
 
-let totalConImpuestos = 0;
-
-function sumarImpuestos(precio, impuesto) {
-    return totalConImpuestos += precio * (1+impuesto / 100)
+//METODO DE PROPIEDAD 
+// SON FUNCIONES CON LA SINTAXIS DE METODO
+// REPRODUCTOR DE MUSICA
+//OBJETO
+let reproductor = {
+    reproducir : function(nc){
+        console.log(`Reproduciendo Cancion con ID: ${nc}`)
+    },
+    pausar : function(){
+        console.log("Pausando Cancion...")
+    },
+    crearPlayList : function(nombre){
+        console.log(`Crando Playlist: ${nombre}`)
+    }
 }
 
-sumarImpuestos(200, 10);
-sumarImpuestos(300, 21);
-sumarImpuestos(500, 5);
+reproductor.reproducir (123);
+reproductor.pausar();
+reproductor.crearPlayList("Rock");
 
-console.log("EL TOTAL CON IMPUESTOS ES DE:", totalConImpuestos)
+// AÑADIR PROPIEDADES A UNA FUNCION 
+reproductor.borrarCancion = function (id){
+    console.log(`Borra cancion: ${id}`);
+}
+
+reproductor.borrarCancion (12);
+
+// Ejercicio expresión de la funcion lo asimila a una variable a la hora de la lectura
+
+let triplicar = function(x){
+   return 3*x ;  //Si no se le indica el return, no se muestra en consola, solo guarda el valor
+}
+
+console.log(triplicar(6))
+
+
+// ARROW FUNCTION / FUNCIONES DE FLECHA 
+
+// SI SOLO HAY UNA LINEA DE CODIGO LAS LLAVES {} SE QUITAN
+// () SI SOLO TIENE UNA ARGUMENTO Y NO ESTA INICIALIZADO NO SE PONE ej : = (numero=1) --- si va porque esta inicializado
+
+let triplicar2 = y => 3*y ;
+
+// EJERCICIO CREAMOS UN ARROW FUNCTION - APRENDIENDO - QUE DEVUELVA UN NUMERO + 7 Y LO ALMACENE EN UNA VARIABLE resultadoAprendiendo
+
+let aprendiendo = numero => numero + 7 ; // numero es el argumento
+let resultadoAprendiendo = aprendiendo(10); // --- creando variable que almacena
+
+console.log(resultadoAprendiendo);
+
+///
+
+
+let carrito = [
+    obje1={
+        name: "TV",
+        costo : 750
+    },
+    obje2={
+    name: "Tablet",
+        costo : 350
+    },
+    obje3={
+    name: "Movil",
+        costo : 550
+    },
+    obje4={
+    name: "PC",
+        costo : 1750
+    }];
+
+console.table(carrito);
+
+// SOME
+rest = carrito.some(producto => producto.name === "Tablet");
+console.log("RESULTADO DE FUNCION SOME IDEAL CON FUNCION DE FLECHA: ", rest);
+
+// REDUCE
+
+rest = carrito.reduce((total, producto) => total + producto.costo , 0);
+
+console.log("REDUCE",rest);
+
+//FILTAR 
+
+rest = carrito.filter (producto => producto.costo > 400); 
+console.log("RESULTADO FILTRACION EN ARROW: ", rest);
+
+// EJERCICIO ------------------------------------------------------------
+//DECLARACION
+function sumaDeclaracion(x1, x2, x3){
+    return x1 + x2 + x3
+}
+
+sumaDeclaracion(1,2,3);
+//EXPRESION
+let sumaExpresion = function(n1, n2, n3){
+    console.log(n1+n2+n3)
+}
+sumaExpresion(10,20,30)
+
+//ARROW
+let sumaFlech = (x1,x2,x3) =>  console.log(x1 + x2 + x3);
+
+sumaFlech (1,6,3);
+
+
+// EJERCICIO -------------------------------------------
+ numeros=[10,9,8,7];
+//Declaracion
+ function buscarDeclaracion (array, x){
+    return array.includes(x)
+ }
+ console.log(buscarDeclaracion(numeros,10));
+
+//BUSCAR EXPRESION
+ let buscarExpresion = function (array, x){
+    return array.includes(x)
+ }
+ console.log(buscarExpresion(numeros,5));
+//ARROW
+ let buscarFlecha = (array, x ) => array.includes(x);
+ console.log(buscarFlecha(numeros,20));
+
+//  EJERCICIO ------------------------------------
+
+let ejercicioFlecha = (q1,q2) => (q1 + q2) * 10;
+
+console.log(ejercicioFlecha(10,23));
+
+
+//EJERCICIO-----------------
+ 
+let parametrosSumados = (paramentro1, parametro2) => paramentro1.length + parametro2.length;
+
+console.log(parametrosSumados("sda","dsa"));
+console.log(parametrosSumados("sdda","dssa"));
+console.log(parametrosSumados("sddss","dssas"));
+
+
+
+arrayE = [1,2,3,4,5,6];
+
+let eliminacion = (arrayEliminatorio) => arrayEliminatorio.pop();
+
+console.log(eliminacion(arrayE))
+
+console.log(arrayE)
