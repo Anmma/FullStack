260668a5676f7900904d77fc91998b9daf7f350d
Ana Maza <mazaromanana@gmail.com>
2023-10-31 20:05:42 +0100

    31102023

---
+++ proyectoPersonal/jS/01.js
@@ -0,0 +1,18 @@
+
+
+document.getElementById("loginA").addEventListener("click" , function(){
+    document.querySelector(".popUpA").style.display = "flex";
+})
+
+document.getElementById("cierre").addEventListener("click" , function(){
+    document.querySelector(".popUpA").style.display = "none"
+})
+
+
+document.getElementById("loginB").addEventListener("click" , function(){
+    document.querySelector(".popUpB").style.display = "flex";
+})
+
+document.getElementById("cierreb").addEventListener("click" , function(){
+    document.querySelector(".popUpB").style.display = "none"
+})
